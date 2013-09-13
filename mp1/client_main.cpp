94f844d81fe140336bc43fc5f642460a7a70ae69
Genevieve LaBelle <glabelle13@gmail.com>
2013-09-13 13:19:37 -0500

    Integrated program options

--- mp1/client_main.cpp
+++ mp1/client_main.cpp
@@ -1,33 +1,64 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 #include "client_main.h"
 #include "client.h"
 #include "types.h"
 #include "util.hpp"
 
-// TODO: implement command arguments to support:
-// 1. pattern, required
-// 2. log file list, required
-// 3. configuration file (contains machine IP/hostname, optional)
-//
+using std::string;
+using namespace boost;
+namespace po = boost::program_options;
+
 // sample usage:
-// ./dgrep [-c conf] -p "pattern" machine.1.log [machine.2.log ...]
+// 
 int main(int argc, char *argv[]) {
-  
+
   // Parse log file list
-  // TODO: Need syntax checker (coz MP1 doc says naming is "machine.i.log")
   try {
+
+    string pattern;
+    vector<string> filenames;
+    string config;
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help", "produce help message")
+      ("config,c", po::value< string >(&config)->
+       default_value("config.txt"), "configuration file (optional)")
+      ("pattern,p", po::value< string >(&pattern), "pattern")
+      ("filenames", po::value< vector<string> >(&filenames),
+       "log files to be searched")
+      ;
+
+    po::positional_options_description p;
+    p.add("filenames", -1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).
+        options(desc).positional(p).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help") || !vm.count("pattern") || !vm.count("filenames")) {
+      cout << "Usage: ./dgrep [-c conf] -p \"pattern\" machine.1.log [machine.2.log ...]\n";
+      cout << desc;
+      return 1;
+    }
+
+    cout << "using config file: " << config << "\n";
     boost::asio::io_service io_service;
     tcp::resolver resolver(io_service);
 
     const char* hosts[] = {
-                        "localhost",
-                        "linux-v1.ews.illinois.edu",
-                        "linux-a1.ews.illinois.edu",
-                        "linux-a2.ews.illinois.edu",
-                        "linux-a3.ews.illinois.edu",
-                       };
+      "localhost",
+      "linux-v1.ews.illinois.edu",
+      "linux-a1.ews.illinois.edu",
+      "linux-a2.ews.illinois.edu",
+      "linux-a3.ews.illinois.edu",
+    };
+
     const int NSERVERS = sizeof(hosts) / sizeof(char*);
     std::vector<ClientPtr> clients;
     std::string pattern("zigang");
@@ -38,13 +69,11 @@ int main(int argc, char *argv[]) {
       std::string filename;
       filename = "machine." + to_string(i) + ".log";
       clients.push_back(ClientPtr(new Client(filename, pattern,
-                                          io_service, iterator)));
+              io_service, iterator)));
     }
 
     io_service.run();
   } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
-
-  return 0;
 }
