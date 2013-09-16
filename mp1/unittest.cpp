b3f3000428e092bdacf65988bb2a8fb982ca377e
Zigang Xiao <iveney@gmail.com>
2013-09-16 02:00:25 -0500

    Wrote a test.

--- mp1/unittest.cpp
+++ mp1/unittest.cpp
@@ -1,18 +1,31 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
 #include "gtest/gtest.h"
+#include "query.h"
+#include "client.h"
+#include "name_parser.h"
+
+using namespace std;
 
 namespace {
 
-// The fixture for testing class Foo.
-class FooTest : public ::testing::Test {
+// the following tests assumes "provided" is linked to "logs/run".
+// golden file under "logs/run/golden"
+class GrepTest : public ::testing::Test {
  protected:
-  // You can remove any or all of the following functions if its body
-  // is empty.
-
-  FooTest() {
-    // You can do set-up work for each test here.
+  GrepTest() {
+    // read the configuration file
+    string config = "config";
+    NameParser nparser(config);
+    hosts_  = nparser.hosts();
+    port_   = "12345";
+    golden_ = "logs/run/golden";
+    null_.open("/dev/null");
   }
 
-  virtual ~FooTest() {
+  virtual ~GrepTest() {
     // You can do clean-up work that doesn't throw exceptions here.
   }
 
@@ -30,16 +43,40 @@ class FooTest : public ::testing::Test {
   }
 
   // Objects declared here can be used by all tests in the test case for Foo.
+  vector<string> hosts_;  // host mappings
+  string port_;
+  string golden_;
+  ofstream null_;
 };
 
-// Tests that the Foo::Bar() method does Abc.
-TEST_F(FooTest, MethodBarDoesAbc) {
-  EXPECT_EQ(0, 0);
+// grep the keys from two hosts
+TEST_F(GrepTest, GrepKeyTest) {
+  string key = "two";
+  Query query[] = {Query(),
+                   Query(Query::KEY, key, "machine.1.log"),
+                   Query(Query::KEY, key, "machine.2.log")};
+
+  // create connections to each servers
+  boost::asio::io_service io_service;
+  vector<ClientPtr> clients;
+  clients.push_back(
+      ClientPtr(new Client(query[1], hosts_[1], port_, io_service, null_)));
+  clients.push_back(
+      ClientPtr(new Client(query[2], hosts_[2], port_, io_service, null_)));
+  io_service.run();
+
+  // read in golden files
+  string prefix = golden_ + "/GrepKeyTest";
+  string f1 = prefix + "/1";
+  string f2 = prefix + "/2";
+  ifstream ifs1(f1.c_str()), ifs2(f2.c_str());
+  stringstream gold[2];
+  gold[0] << ifs1.rdbuf();
+  gold[1] << ifs2.rdbuf();
+
+  // compare with golden
+  EXPECT_EQ(clients[0]->result(), gold[0].str());
+  EXPECT_EQ(clients[1]->result(), gold[1].str());
 }
 
-// Tests that Foo does Xyz.
-TEST_F(FooTest, DoesXyz) {
-  // Exercises the Xyz feature of Foo.
-}
-
-}  // namespace
+} // namespace
